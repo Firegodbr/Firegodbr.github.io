2840abd536f6c5c8074f8cd7d3cee8fd49086eff
Rogerio Ferreira de Melo <rogeriobr740@live.com>
2025-09-10 11:27:03 -0400

    hotfix: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // base: '/'
+  base: '/Firegodbr.github.io',
+  build: {
+    outDir: 'dist'
+  }
 })
