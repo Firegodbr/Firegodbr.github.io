22d2aeb4a4adab390e4b11de4f1093d4ab9e6249
Rogerio Ferreira de Melo <rogeriobr740@live.com>
2025-09-10 11:48:59 -0400

    Revert "feat: github actions"
    This reverts commit 1f619c0b9d2cc449046f54cc1e3524f57c911da2.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',
+  base: '/Firegodbr.github.io',
   build: {
     outDir: 'dist'
   }
